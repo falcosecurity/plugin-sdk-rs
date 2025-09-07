f3e8609e06ec43abc8fc0fbb4b4b17afc0208f1c
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-09-07 14:07:47 +0200

    chore(plugin): move tables API module into separate files
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

---
+++ falco_plugin/src/tables/export/mod.rs
@@ -0,0 +1,73 @@
+//! Exporting tables to other plugins
+//!
+//! Exporting a table to other plugins is done using the [`Entry`] derive macro.
+//! It lets you use a struct type as a parameter to [`Table`]. You can then create
+//! a new table using [`TablesInput::add_table`](crate::tables::TablesInput::add_table).
+//!
+//! Every field in the entry struct must be wrapped in [`Public`](`crate::tables::export::Public`),
+//! [`Private`](`crate::tables::export::Private`) or [`Readonly`](`crate::tables::export::Readonly`),
+//! except for nested tables. These just need to be a `Box<Table<K, E>>`, as it makes no sense
+//! to have a private nested table and the distinction between writable and readonly is meaningless
+//! for tables (they have no setter to replace the whole table and you can always add/remove
+//! entries from the nested table).
+//!
+//! # Example
+//!
+//! ```
+//! use std::ffi::{CStr, CString};
+//! use falco_plugin::base::Plugin;
+//!# use falco_plugin::plugin;
+//! use falco_plugin::tables::TablesInput;
+//! use falco_plugin::tables::export;
+//!
+//! // define the struct representing each table entry
+//! #[derive(export::Entry)]
+//! struct ExportedTable {
+//!     int_field: export::Readonly<u64>,      // do not allow writes via the plugin API
+//!     string_field: export::Public<CString>, // allow writes via the plugin API
+//!     secret: export::Private<Vec<u8>>,      // do not expose over the plugin API at all
+//! }
+//!
+//! // define the type holding the plugin state
+//! struct MyPlugin {
+//!     // you can use methods on this instance to access fields bypassing the Falco table API
+//!     // (for performance within your own plugin)
+//!     exported_table: Box<export::Table<u64, ExportedTable>>,
+//! }
+//!
+//! // implement the base::Plugin trait
+//! impl Plugin for MyPlugin {
+//!     // ...
+//!#     const NAME: &'static CStr = c"sample-plugin-rs";
+//!#     const PLUGIN_VERSION: &'static CStr = c"0.0.1";
+//!#     const DESCRIPTION: &'static CStr = c"A sample Falco plugin that does nothing";
+//!#     const CONTACT: &'static CStr = c"you@example.com";
+//!#     type ConfigType = ();
+//!
+//!     fn new(input: Option<&TablesInput>, config: Self::ConfigType)
+//!         -> Result<Self, anyhow::Error> {
+//!
+//!         let Some(input) = input else {
+//!             anyhow::bail!("Did not get tables input");
+//!         };
+//!
+//!         // create a new table called "exported"
+//!         //
+//!         // The concrete type is inferred from the field type the result is stored in.
+//!         let exported_table = input.add_table(export::Table::new(c"exported")?)?;
+//!
+//!         Ok(MyPlugin { exported_table })
+//!     }
+//! }
+//!# plugin!(#[no_capabilities] MyPlugin);
+//! ```
+
+pub use crate::plugin::exported_tables::field::private::Private;
+pub use crate::plugin::exported_tables::field::public::Public;
+pub use crate::plugin::exported_tables::field::readonly::Readonly;
+pub use crate::plugin::exported_tables::table::Table;
+
+/// Mark a struct type as a table value
+///
+/// See the [module documentation](`crate::tables::export`) for details.
+pub use falco_plugin_derive::Entry;
---
+++ falco_plugin/src/tables/mod.rs
@@ -0,0 +1,150 @@
+//! # Creating and accessing tables
+//!
+//! Tables are a mechanism to share data between plugins (and Falco core). There are three major
+//! concepts that relate to working with Falco plugin tables:
+//! - a table is a collection of entries, each under a different key, like a hash map or a SQL
+//!   table with a single primary key
+//! - an entry is a struct containing the actual values (corresponding to an entry in the hash map
+//!   or a row in the SQL table)
+//! - a field is a descriptor for a particular item in an entry. It does not have an equivalent
+//!   in the hash map analogy, but corresponds to a column in the SQL table. In particular, a field
+//!   is not attached to any particular entry.
+//!
+//! ## Example (in pseudocode)
+//!
+//! Consider a table called `threads` that has two fields:
+//! ```ignore
+//! struct Thread {
+//!     uid: u64,
+//!     comm: CString,
+//! }
+//! ```
+//!
+//! and uses the thread id (`tid: u64`) as the key. To read the `comm` of the thread with tid 1,
+//! you would need the following operations:
+//!
+//! ```ignore
+//! // get the table (at initialization time)
+//! let threads_table = get_table("threads");
+//!
+//! // get the field (at initialization time)
+//! let comm_field = threads_table.get_field("comm");
+//!
+//! // get an entry in the table (during parsing or extraction)
+//! let tid1_entry = threads_table.get_entry(1);
+//!
+//! // get the field value from an entry
+//! let comm = tid1_entry.get_field_value(comm_field);
+//! ```
+//!
+//! The Rust SDK tries to hide this and expose a more struct-oriented approach, though you can
+//! access fields in entries manually if you want (e.g. if you only know the field name at runtime).
+//!
+//! # Supported field types
+//!
+//! The following types can be used in fields visible over the plugin API:
+//! - integer types (u8/i8, u16/i16, u32/i32, u64/i64)
+//! - the bool type
+//! - CString
+//!
+//! Any other types are not supported, including in particular e.g. collections (`Vec<T>`),
+//! enums or any structs.
+//!
+//! # Nested tables
+//!
+//! Fields can also have a table type. This amounts to nested tables, like:
+//! ```ignore
+//! let fd_type = threads[tid].file_descriptors[fd].fd_type;
+//! ```
+//!
+//! One important limitation is that you cannot add a nested table at runtime, so the only
+//! nested tables that exist are defined by the plugin (or Falco core) which owns the parent table.
+//!
+//! # Exporting and importing tables
+//!
+//! Tables can be exported (exposed to other plugins) using the [`export`] module.
+//!
+//! Existing tables (from other plugins) can be imported using the [`import`] module.
+//!
+//! See the corresponding modules' documentation for details.
+//!
+//! # Access control
+//!
+//! Not all plugins are created equal when it comes to accessing tables. Only
+//! [parse plugins](`crate::parse::ParsePlugin`), [listen plugins](`crate::listen::CaptureListenPlugin`)
+//! and [extract plugins](`crate::extract::ExtractPlugin`) can access tables. Moreover, during field
+//! extraction you can only read tables, not write them.
+//!
+//! To summarize:
+//!
+//! | Plugin type | Initialization phase | Action phase ^1 |
+//! |-------------|----------------------|-----------------|
+//! | source      | no access            | no access       |
+//! | parse       | full access          | read/write      |
+//! | extract     | full access ^2       | read only       |
+//! | listen      | full access          | n/a ^3          |
+//! | async       | no access            | no access       |
+//!
+//! **Notes**:
+//! 1. "Action phase" is anything that happens after [`crate::base::Plugin::new`] returns, i.e.
+//!    event generation, parsing/extraction or any background activity (in async plugins).
+//!
+//! 2. Even though you can create tables and fields during initialization of an extract plugin,
+//!    there's no way to modify them later (create table entries or write to fields), so it's
+//!    more useful to constrain yourself to looking up existing tables/fields.
+//!
+//! 3. Listen plugins don't really have an action phase as they only expose methods to run
+//!    on capture start/stop. The routines they spawn cannot access tables, since the table
+//!    API is explicitly not thread safe (but with the `thread-safe-tables` feature you can
+//!    safely access tables from Rust plugins across many threads).
+//!
+//! ## Access control implementation
+//!
+//! Access control is implemented by requiring a particular object to actually perform table
+//! operations:
+//! - [`TablesInput`] to manage (look up/create) tables and fields
+//! - [`TableReader`] to look up table entries and get field values
+//! - [`TableWriter`] to create entries and write field values
+//!
+//! These get passed to your plugin whenever a particular class of operations is allowed.
+//! Note that [`crate::base::Plugin::new`] receives an `Option<&TablesInput>` and the option
+//! is populated only for parsing and extraction plugins (source and async plugins receive `None`).
+//!
+//! # The flow of using tables
+//!
+//! The access controls described above push you into structuring your plugins in a specific way.
+//! You cannot e.g. define tables in a source plugin, which is good, since that would break
+//! when reading capture files (the source plugin is not involved in that case). To provide
+//! a full-featured plugin that generates events, maintains some state and exposes it via
+//! extracted fields, you need separate capabilities (that may live in a single plugin or be
+//! spread across different ones):
+//! - a source plugin *only* generates events
+//! - a parse plugin creates the state tables and updates them during event parsing
+//! - an extract plugin reads the tables and returns field values
+//!
+//! # Dynamic fields
+//!
+//! Tables can have fields added to them at runtime, from other plugins than the one that
+//! created them (you can add dynamic fields to tables you created too, but that makes little sense).
+//!
+//! These fields behave just like fields defined statically in the table and can be used by plugins
+//! loaded after the current one. This can be used to e.g. add some data to an existing table
+//! in a parse plugin and expose it in an extract plugin.
+//!
+//! # Thread safety
+//!
+//! Tables in the Falco plugin API are explicitly *not* thread safe. However, when you enable
+//! the `thread-safe-tables` feature, tables exported from your plugin become thread-safe, so you
+//! can use them from your plugin (e.g. in a separate thread) concurrently to other plugins
+//! (in the main thread).
+
+pub use crate::plugin::tables::vtable::reader::LazyTableReader;
+pub use crate::plugin::tables::vtable::reader::TableReader;
+pub use crate::plugin::tables::vtable::reader::ValidatedTableReader;
+pub use crate::plugin::tables::vtable::writer::LazyTableWriter;
+pub use crate::plugin::tables::vtable::writer::TableWriter;
+pub use crate::plugin::tables::vtable::writer::ValidatedTableWriter;
+pub use crate::plugin::tables::vtable::TablesInput;
+
+pub mod export;
+pub mod import;
