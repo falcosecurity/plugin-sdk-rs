53c1b4851f24c3e40aaf63588b2c84030fe9052e
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2025-09-09 21:49:57 +0200

    chore(plugin): make tables::export::ref_shared private
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- falco_plugin/src/tables/export/macros.rs
+++ falco_plugin/src/tables/export/macros.rs
@@ -3,7 +3,6 @@
 macro_rules! table_export_expose_internals {
     () => {
         pub mod export {
-            pub use $crate::tables::export::ref_shared::RefShared;
             pub use $crate::tables::export::traits::Entry;
             pub use $crate::tables::export::traits::TableMetadata;
             pub use $crate::tables::export::DynamicFieldValue;
@@ -12,6 +11,7 @@ macro_rules! table_export_expose_internals {
             pub use $crate::tables::export::FieldRef;
             pub use $crate::tables::export::HasMetadata;
             pub use $crate::tables::export::Metadata;
+            pub use $crate::tables::export::RefShared;
 
             pub use $crate::tables::export::static_field_specialization::StaticFieldCheck;
             pub use $crate::tables::export::static_field_specialization::StaticFieldFallback;
